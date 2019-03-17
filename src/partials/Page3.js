8d196008f54c6b4e40528cd6e91959d706521a33
Kevin Wang <kwangsan@gmail.com>
2019-03-17 12:04:21 -0400

    layout Page3 & Page4

--- src/partials/Page3.js
+++ src/partials/Page3.js
@@ -1,5 +1,9 @@
 import * as React from "react"
 import { partials, leftPage, pageTabLow } from "../shared/styles"
+import { COLLAGE } from "../images/Page3-collage"
+import { RESIDENCE } from "../images/Page3-residence"
+
+const MARGIN = 30
 
 export default function Page3() {
   return (
@@ -8,6 +12,64 @@ export default function Page3() {
         <div className="pageTab-date">180731</div>
         <div className="pageTab-number">CHENG, page 3</div>
       </div>
+
+      <div className="row" style={{ marginTop: MARGIN }}>
+        <div className="image" style={{}}>
+          {COLLAGE}
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          top: 0,
+          left: 0,
+          marginLeft: MARGIN,
+          marginRight: MARGIN,
+          marginTop: MARGIN,
+        }}
+      >
+        <div className="image" style={{ width: `60%` }}>
+          {RESIDENCE}
+        </div>
+        <div className="text" style={{ width: `40%`, marginLeft: MARGIN }}>
+          {TEXT}
+        </div>
+      </div>
     </div>
   )
 }
+
+const TEXT = (
+  <div style={{ fontSize: 9.7, lineHeight: 1 }}>
+    CDE Residence Hall <br />
+    Williams College, Massachusetts
+    <br />
+    Education, Residential
+    <br />
+    <br />
+    Project Team:
+    <br />
+    Samuel G. White, Anne H. Smith, Serena Loconczy, Rena Mande, Thomas, Faust,
+    Taryn Cook, Connie Cheng, Paul Appleby, Alex Alaimo
+    <br />
+    <br />
+    Original rendering (left) credits to Scott Baumberger & Platt Byard Dovell
+    White Architects.
+    <br />
+    <br />
+    •••••••••••••••••••••••••••••••••••••••
+    <br />
+    <br />
+    The Center for Development Economics (CDE) at Williams College offers a one
+    year master degree program to international students from low and middle
+    income countries. PBDW was asked to design a new residence hall for the CDE,
+    thus allowing for the distinction of living and learning spaces that the
+    organization currently lacks.
+    <br />
+    <br />
+    continued >>
+  </div>
+)
--- src/partials/Page4.js
+++ src/partials/Page4.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import { partials, rightPage, pageTabLow } from "../shared/styles"
+import { COLLAGE } from "../images/Page4-collage"
+
+const MARGIN = 30
 
 export default function Page4() {
   return (
@@ -11,6 +14,55 @@ export default function Page4() {
         <div className="pageTab-date">180731</div>
         <div className="pageTab-number">CHENG, page 4</div>
       </div>
+
+      <div className="row" style={{ marginTop: MARGIN }}>
+        <div className="image" style={{}}>
+          {COLLAGE}
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          top: 0,
+          left: 0,
+        }}
+      >
+        <div className="text" style={{ width: `40%`, margin: MARGIN }}>
+          {TEXT}
+        </div>
+      </div>
     </div>
   )
 }
+
+const TEXT = (
+  <div style={{ fontSize: 9.7, lineHeight: 1 }}>
+    I had been a part of the design team since the schematic design phase,
+    during which I worked on materials for presentations and developed the 3D
+    model in Revit.
+    <br />
+    <br />
+    Due to a sensitive budget, campus specific requirements, and net zero energy
+    goals, the design underwent several rounds of value engineering - I helped
+    to ensure that documents reflected corresponding changes.
+    <br />
+    <br />
+    I was responsible for parts of the construction documents, including
+    interior elevations, typical power/finish/reflected-ceiling plans, and
+    material selections.
+    <br />
+    <br />
+    I enjoyed working together with other team memebrs to review submittals and
+    shop drawings and to respond to requests for information.
+    <br />
+    <br />
+    I enjoyed working together with other team members to review submittals and
+    shop drawings and to respond to requests for information.
+    <br />
+    <br />
+    The residence hall is slated for completed this fall.
+  </div>
+)
