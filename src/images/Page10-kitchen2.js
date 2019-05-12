4125fc489706b5823749cdfd9b932be2d8b41ed3
Kevin Wang <kwangsan@gmail.com>
2019-05-12 18:50:30 -0400

    add images for Page9 & Page10 - jpg files - StaticQuery js files

---
+++ src/images/Page10-kitchen2.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export const KITCHEN2 = (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "Page10-kitchen2.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 900) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+  />
+)
