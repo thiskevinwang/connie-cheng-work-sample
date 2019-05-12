60d7e2aa4b2d7dcda83ff2c4c31730186b5d3614
Kevin Wang <kwangsan@gmail.com>
2019-05-12 18:33:26 -0400

    update scroll helper message

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,11 +27,19 @@ const Layout = ({ children }) => (
       <>
         {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
         {children}
-        <div style={{ position: "fixed", color: "lightgray", fontSize: 12 }}>
-          site built by:{" "}
-          <a style={{ color: "lightgray" }} href="https://www.thekevinwang.com">
-            @thekevinwang
-          </a>
+        <div
+          className={"helper"}
+          style={{
+            position: "fixed",
+            left: 0,
+            transform: `translate(100px, 300px)`,
+            color: "black",
+            fontSize: 16,
+            fontFamily: "GENISO",
+            zIndex: -1,
+          }}
+        >
+          Feel free to navigate with the left & right arrow keys!
         </div>
       </>
     )}
